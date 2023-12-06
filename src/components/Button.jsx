1fd71ec6cb98b9600d92da907fc9c7c220b866ca
Mohamed-78 <dieudonneirie7@gmail.com>
2023-12-06 10:05:14 +0000

    Fin des 3 avant derniers elements

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
-export const Button = ({label, iconURL}) => {
+export const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className="flex justify-center items-center 
-    gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-    bg-coral-red rounded-full text-white border-coral-red">
-       {label}
-       <img src={iconURL} className="ml-2 rounded-full w-5 h-5"/>
+    <button className={`flex justify-center items-center 
+      gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor 
+        ? `${backgroundColor} $
+        {textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+        } rounded-full w-full"}`}
+      >
+      {label}
+      {iconURL && <img src={iconURL} className="ml-2 rounded-full w-5 h-5"/>}
     </button>
   )
 }
